b8a5a7fbe3b8fe5ff2a557ec1bb47a33eae10d2c
Simón Ccorimanya Ortiz de Orue <lvpccori@gmail.com>
2023-07-08 22:52:41 -0500

    Se arreglo el modelo de Erwin y se agrego data para crear la BD y cargar datos a la base

--- DatosQuerry.sql
+++ DatosQuerry.sql
@@ -1,23 +1,52 @@
---Tabla Logins
-insert into Logins values ('test', '1234')
+use FastFood
+go
 
+--Tabla Logins
+insert into Logins (Username, Password)
+VALUES
+('test', '1234')
+GO
 --Tabla de Proveedores
-insert into Proveedores values ('ID001', 'Proveedor 1', 'Direccion 1', 31233);
-insert into Proveedores values ('ID002', 'Proveedor 2', 'Direccion 2', 123456);
-insert into Proveedores values ('ID003', 'Proveedor 3', 'Direccion 3', 0001122);
+insert into Proveedores (ID_Proveedor, Nombre_Proveedor, RUC_RS, Telefono)
+values
+('ID001', 'Proveedor 1', 'Direccion 1', 31233),
+('ID002', 'Proveedor 2', 'Direccion 2', 123456),
+('ID003', 'Proveedor 3', 'Direccion 3', 0001122)
+GO
 
 --Tabla de Empleados
-insert into Empleados values ('E001', 'Empleado 1', 123456, 'Correo 1', '2023-07-07');
-insert into Empleados values ('E002', 'Empleado 2', 123456, 'Correo 2', '2023-07-07');
-insert into Empleados values ('E003', 'Empleado 3', 123456, 'Correo 3', '2023-07-07');
+insert into Empleados (ID_Empleado, Nombre, Telefono, Correo, Fecha_Cumple)
+VALUES
+('E001', 'Empleado 1', 123456, 'Correo 1', '2023-07-07'),
+('E002', 'Empleado 2', 123456, 'Correo 2', '2023-07-07'),
+('E003', 'Empleado 3', 123456, 'Correo 3', '2023-07-07')
+go
 
 --Tabla de Productos
-insert into Productos values ('P001', 'Producto 1', 10, 12, 100, '');
-insert into Productos values ('P002', 'Producto 2', 20, 22, 100, '');
-insert into Productos values ('P003', 'Producto 3', 30, 32, 100, '');
-insert into Productos VALUES ('P004', 'Producto 4', 40, 42, 100, '');
+INSERT INTO Productos (ID_Producto, Nombre_Producto, Costo, Precio_Venta, Stock)
+VALUES 
+('P001', 'Pollo crispy', 20, 24, 79),
+('P002', 'Salchipapa', 15, 18, 2),
+('P003', 'Burger clasica chicken', 15, 18, 42),
+('P004', 'Burguer clasica meat', 15, 18, 82),
+('P005', 'Choripan', 12, 14.4, 94),
+('P006', 'Gaseosa', 4.5, 5.4, 85),
+('P007', 'Agua', 3, 3.6, 33),
+('P008', 'Vaso de chicha', 3, 3.6, 73),
+('P009', 'Vaso de maracuya', 3, 3.6, 97)
+GO
 
 
 --Tabla de Compra Detalle
-insert into Compra_Detalle values ('C0001', 'P001', 10, 100, 1000);
-insert into Compra_Detalle values ('C0001', 'P002', 20, 200, 2000);
+insert into Compra_Detalle (Cod_Compra, ID_Producto, Cantidad, Coste_Unitario, Total)
+VALUES
+('C0001', 'P001', 10, 100, 1000),
+('C0001', 'P002', 20, 200, 2000),
+('C0002', 'P003', 30, 300, 3000),
+('C0002', 'P004', 40, 400, 4000),
+('C0003', 'P005', 50, 500, 5000),
+('C0003', 'P006', 60, 600, 6000),
+('C0003', 'P007', 70, 700, 7000),
+('C0003', 'P008', 80, 800, 8000),
+('C0003', 'P009', 90, 900, 9000)
+GO
---
+++ FastFooddb.sql
@@ -0,0 +1,256 @@
+
+USE master
+go
+
+
+
+DROP DATABASE FastFood
+go
+
+
+
+CREATE DATABASE FastFood
+go
+
+
+
+USE FastFood
+go
+
+
+
+CREATE TABLE Carrete_Pedidos
+( 
+	ID_Venta             varchar(20)  NOT NULL ,
+	TimeStamp            datetime2(0)  NOT NULL ,
+	Estado               varchar(20)  NOT NULL 
+)
+go
+
+
+
+ALTER TABLE Carrete_Pedidos
+	ADD CONSTRAINT XPKCarrete_Pedidos PRIMARY KEY  CLUSTERED (ID_Venta ASC)
+go
+
+
+
+CREATE TABLE Compra_Detalle
+( 
+	ID_Producto          varchar(20)  NOT NULL ,
+	Cantidad             decimal(10,2)  NULL ,
+	Coste_Unitario       decimal(10,2)  NULL ,
+	Total                decimal(10,2)  NOT NULL ,
+	Cod_Compra           varchar(20)  NULL 
+)
+go
+
+
+
+CREATE TABLE Compra_Total
+( 
+	Fecha                datetime2(0)  NOT NULL ,
+	Cod_Compra           varchar(20)  NOT NULL ,
+	Total                decimal(10,2)  NOT NULL ,
+	ID_Proveedor         varchar(20)  NOT NULL ,
+	ID_Empleado          varchar(20)  NOT NULL 
+)
+go
+
+
+
+ALTER TABLE Compra_Total
+	ADD CONSTRAINT XPKCompra_Total PRIMARY KEY  CLUSTERED (Cod_Compra ASC)
+go
+
+
+
+CREATE TABLE Empleados
+( 
+	ID_Empleado          varchar(20)  NOT NULL ,
+	Nombre               varchar(20)  NOT NULL ,
+	Telefono             int  NULL ,
+	Correo               varchar(20)  NULL ,
+	Fecha_Cumple         date  NULL 
+)
+go
+
+
+
+ALTER TABLE Empleados
+	ADD CONSTRAINT XPKEmpleados PRIMARY KEY  CLUSTERED (ID_Empleado ASC)
+go
+
+
+
+CREATE TABLE Logins
+( 
+	Username             varchar(20)  NOT NULL ,
+	Password             varchar(20)  NOT NULL 
+)
+go
+
+
+
+ALTER TABLE Logins
+	ADD CONSTRAINT XPKLogins PRIMARY KEY  CLUSTERED (Username ASC)
+go
+
+
+
+CREATE TABLE Productos
+( 
+	ID_Producto          varchar(20)  NOT NULL ,
+	Nombre_Producto      varchar(20)  NOT NULL ,
+	Costo                decimal(10,2)  NOT NULL ,
+	Precio_Venta         decimal(10,2)  NOT NULL ,
+	Stock                decimal(10,2)  NULL ,
+)
+go
+
+
+
+ALTER TABLE Productos
+	ADD CONSTRAINT XPKProductos PRIMARY KEY  CLUSTERED (ID_Producto ASC)
+go
+
+
+
+CREATE TABLE Proveedores
+( 
+	ID_Proveedor         varchar(20)  NOT NULL ,
+	Nombre_Proveedor     varchar(40)  NOT NULL ,
+	RUC_RS               varchar(20)  NULL ,
+	Telefono             int  NULL 
+)
+go
+
+
+
+ALTER TABLE Proveedores
+	ADD CONSTRAINT XPKProveedores PRIMARY KEY  CLUSTERED (ID_Proveedor ASC)
+go
+
+
+
+CREATE TABLE Receta
+( 
+	Precio_Venta         decimal(10,2)  NOT NULL ,
+	Nombre               varchar(40)  NOT NULL ,
+	ID_Receta            varchar(20)  NULL 
+)
+go
+
+
+
+CREATE TABLE Receta_Insumo
+( 
+	ID_Recetas           varchar(20)  NOT NULL ,
+	Cantidad             decimal(10,2)  NOT NULL ,
+	Unidad               varchar(10)  NOT NULL ,
+	ID_Producto          varchar(20)  NULL 
+)
+go
+
+
+
+CREATE TABLE Ventas
+( 
+	ID_Venta             varchar(20)  NOT NULL ,
+	TimeStamp            datetime2(0)  NOT NULL ,
+	ID_Empleado          varchar(20)  NOT NULL ,
+	Total                decimal(10,2)  NOT NULL ,
+	Metodo_pago          varchar(20)  NOT NULL 
+)
+go
+
+
+
+ALTER TABLE Ventas
+	ADD CONSTRAINT XPKVentas PRIMARY KEY  CLUSTERED (ID_Venta ASC)
+go
+
+
+
+CREATE TABLE Ventas_Detalle
+( 
+	ID_Venta             varchar(20)  NOT NULL ,
+	Cantidad             int  NOT NULL ,
+	Total                decimal(10,2)  NOT NULL ,
+	ID_Receta            varchar(20)  NULL 
+)
+go
+
+
+
+
+ALTER TABLE Carrete_Pedidos
+	ADD CONSTRAINT R_21 FOREIGN KEY (ID_Venta) REFERENCES Ventas(ID_Venta)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+go
+
+
+
+
+ALTER TABLE Compra_Detalle
+	ADD CONSTRAINT R_6 FOREIGN KEY (ID_Producto) REFERENCES Productos(ID_Producto)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+go
+
+
+
+
+ALTER TABLE Compra_Detalle
+	ADD CONSTRAINT R_30 FOREIGN KEY (Cod_Compra) REFERENCES Compra_Total(Cod_Compra)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+go
+
+
+
+
+ALTER TABLE Compra_Total
+	ADD CONSTRAINT R_5 FOREIGN KEY (ID_Proveedor) REFERENCES Proveedores(ID_Proveedor)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+go
+
+
+
+
+ALTER TABLE Compra_Total
+	ADD CONSTRAINT R_25 FOREIGN KEY (ID_Empleado) REFERENCES Empleados(ID_Empleado)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+go
+
+
+
+
+ALTER TABLE Receta_Insumo
+	ADD CONSTRAINT R_23 FOREIGN KEY (ID_Producto) REFERENCES Productos(ID_Producto)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+go
+
+
+
+
+ALTER TABLE Ventas
+	ADD CONSTRAINT R_22 FOREIGN KEY (ID_Empleado) REFERENCES Empleados(ID_Empleado)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+go
+
+
+
+
+ALTER TABLE Ventas_Detalle
+	ADD CONSTRAINT R_14 FOREIGN KEY (ID_Venta) REFERENCES Ventas(ID_Venta)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+go
+
+
